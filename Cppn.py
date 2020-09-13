0e9e8752e216c05bc1ad7f04068485db8cfcf951
saxenabhishek <as7122000@gmail.com>
2020-09-13 12:20:58 +0530

    Initial

---
+++ Cppn.py
@@ -0,0 +1,19 @@
+"""
+Cppn Module
+
+Consillio
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn.functional as F
+
+
+class Cppn:
+    pass
+
+if __name__=="__main__":
+    print("Unit_Test")
+

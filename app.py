112e4469c67f64076cf50b5d232b3ff5ecbf4de3
aryankargwal <kargwalaryan@gmail.com>
2020-09-19 22:57:39 +0530

    Final Hackathon code

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#Imports
 from flask import Flask, render_template
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,13 +10,14 @@ from PIL import Image
 
 app = Flask(__name__)
 
+#For running the model saving a static file of the generated image
 @app.route('/')
 def home():
     M = Cppn()
     r = M(450,600,10)
     im = Image.fromarray(r)
 
-    new_graph_name = "graph" + str(time.time()) + ".png"
+    new_graph_name = "graph" + ".png"
 
     for filename in os.listdir('static/'):
         if filename.startswith('graph'):  # not to remove other images

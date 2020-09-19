113015aa0c9e25b5194fbbeedc903f99d87bf5fe
saxenabhishek <as7122000@gmail.com>
2020-09-19 16:48:22 +0530

    Basic input server side working

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 import numpy as np
 import matplotlib.pyplot as plt
 from Cppn import Cppn
@@ -8,11 +8,12 @@ from PIL import Image
 
 
 app = Flask(__name__)
+M = Cppn()
 
 @app.route('/')
-def home():
-    M = Cppn()
-    r = M(450,600,10)
+def home(h=450,w=600,z=10):
+    r = M(h,w,z)
+    
     im = Image.fromarray(r)
 
     new_graph_name = "Res" + str(time.time()) + ".png"
@@ -25,12 +26,16 @@ def home():
         if filename.startswith('Res'):  # not to remove other images
             os.remove(saveloc + filename)
     
-
     im = im.save(saveloc + new_graph_name) 
 
     return render_template("index.html",graph= "/Dyaimages/"+new_graph_name)
 
-
+@app.route('/gen',methods = ['POST', 'GET'])
+def update():
+    print(request.form)
+    print(request.values)
+    print(request.args.get("name1"))
+    return home(450,600,5)
 
 if __name__ == '__main__':
     app.run(debug=True)
